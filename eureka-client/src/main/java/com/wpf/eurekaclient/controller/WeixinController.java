8b417eb276f324a692a30ffb696d292fe7e95147
wangpengfei <wangpengfei@91taogu.com>
2019-04-03 10:34:27 +0800

    修改微信公众号接入接口验证

--- eureka-client/src/main/java/com/wpf/eurekaclient/controller/WeixinController.java
+++ eureka-client/src/main/java/com/wpf/eurekaclient/controller/WeixinController.java
@@ -3,7 +3,6 @@ package com.wpf.eurekaclient.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qq.weixin.mp.aes.AesException;
@@ -13,13 +12,17 @@ import com.qq.weixin.mp.aes.WXBizMsgCrypt;
 @RequestMapping("/weixin")
 public class WeixinController {
 
-  @RequestMapping(value = "/hello", method = RequestMethod.GET)
+  private String token = "";
+  private String encodingAesKey = "";
+  private String appId = "";
+
+  @RequestMapping(value = "/hello")
   public String home(HttpServletRequest request) throws AesException {
     String signature = request.getParameter("signature");
     String timeStamp = request.getParameter("timestamp");
     String nonce = request.getParameter("nonce");
     String echoStr = request.getParameter("echostr");
-    WXBizMsgCrypt wXBizMsgCrypt = new WXBizMsgCrypt("test123", "L5Xq644Z2wVlhOa9TPTeBAcr2u2Cn1ksm6UgGwS9289", "wx5b07035a88d620a5");
+    WXBizMsgCrypt wXBizMsgCrypt = new WXBizMsgCrypt(token, encodingAesKey, appId);
     return wXBizMsgCrypt.verifyUrl(signature, timeStamp, nonce, echoStr);
   }
 }
